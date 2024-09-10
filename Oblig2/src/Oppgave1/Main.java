211fe28179d620ff0cf19c5b004ff21acb70b551
h676669 <676669@stud.hvl.no>
2024-09-10 15:01:57 +0200

    ferdig med oppg1 på oblig 2

--- Oblig2/src/Oppgave1/Main.java
+++ Oblig2/src/Oppgave1/Main.java
@@ -1,5 +1,34 @@
 package Oppgave1;
+import javax.swing.JOptionPane;
 
-public class Main
-{
+public class Main {
+    public static String Melding;
+    public static void main(String[] args) {
+        Melding = "Hallo Verden!";
+        Thread skrivUtMelding = new Thread(){
+            @Override
+            public void run(){
+                // vil kjøre vist quit blir skrevet mens thread såver
+                while(!Melding.equals("quit")){
+                    try{
+                        Thread.sleep(3000);
+                        if(Melding.equals("quit")){
+                            break;
+                        }
+                        System.out.println(Melding);
+                    }catch(InterruptedException e){}
+                }
+            }
+        };
+        Thread velgMelding = new Thread(){
+            @Override
+            public void run(){
+                while(!Melding.equals("quit")){
+                    Melding = javax.swing.JOptionPane.showInputDialog("Skriv inn melding din, skriv quit for å avslutte");
+                }
+            }
+        };
+        velgMelding.start();
+        skrivUtMelding.start();
+    }
 }
