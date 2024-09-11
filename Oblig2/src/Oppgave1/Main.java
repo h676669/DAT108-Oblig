01f61b88c3b3e08b5f79d0c399bc382ddb0881b9
h676669 <142335301+h676669@users.noreply.github.com>
2024-09-11 07:52:37 +0200

    optimialisering på oppg1

--- Oblig2/src/Oppgave1/Main.java
+++ Oblig2/src/Oppgave1/Main.java
@@ -3,31 +3,42 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static String Melding;
+    public static boolean Kjorrer;
     public static void main(String[] args) {
         Melding = "Hallo Verden!";
+        Kjorrer = true;
         Thread skrivUtMelding = new Thread(){
             @Override
             public void run(){
-                while(!Melding.equals("quit")){
-                    try{
+                while(Kjorrer){
+                    try {
                         Thread.sleep(3000);
-                        if(Melding.equals("quit")){
+                        if(!Kjorrer){
                             break;
                         }
                         System.out.println(Melding);
-                    }catch(InterruptedException e){}
+                    } catch(InterruptedException e){}
                 }
             }
         };
         Thread velgMelding = new Thread(){
             @Override
             public void run(){
-                while(!Melding.equals("quit")){
+                while(Kjorrer){
                     Melding = javax.swing.JOptionPane.showInputDialog("Skriv inn melding din, skriv quit for å avslutte");
+                    if(Melding.equals("quit")){
+                        Kjorrer = false;
+                    }
                 }
             }
         };
         velgMelding.start();
         skrivUtMelding.start();
+
+        try {
+            skrivUtMelding.join();
+            velgMelding.join();
+        } catch (InterruptedException e) {
+        }
     }
 }
