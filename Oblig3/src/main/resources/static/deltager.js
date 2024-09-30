1da49547b74d9cbbf69099c2ac476cff9ddd9e49
h676669 <142335301+h676669@users.noreply.github.com>
2024-09-30 13:50:34 +0200

    fra timen

--- Oblig3/src/main/resources/static/deltager.js
+++ Oblig3/src/main/resources/static/deltager.js
@@ -1,10 +1,30 @@
 class DeltagerManager {
     // Deklarer felt-variabler her
+    #startnummer
+    #navn
+    #sluttid
+    #deltager
+    #buttonEl
+    #hidden
 
     constructor(root) {
         // Legg inn kode her
+        this.root = root;
+        this.#deltager = [];
+        this.#navn = root.querySelector('#navn');
+        this.#sluttid = root.querySelector('#sluttid');
+        this.#startnummer = root.querySelector('#startnummer');
+        this.#buttonEl = root.querySelector('#registrerbutton');
+        this.#hidden = root.querySelector('.hidden');
 
+
+        this.#buttonEl.addEventListener('click',this.#registrerMedlem());
     }
+
+    #registrerMedlem(){
+        this.#hidden.r
+    }
+
 
     // Deklarer klassen sine public og private metoder her
 }
