b52d343b50784dc0afb61f2058c36625c23fe879
h676669 <142335301+h676669@users.noreply.github.com>
2024-10-22 11:01:25 +0200

    gjor oblig 4 til et spring prosjekt

---
+++ Oblig4/src/main/java/org/example/deltagerController.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class deltagerController {
+
+    @GetMapping("/paamelding")
+    public String paamelding(){
+        return "paamelding";
+    }
+
+}
