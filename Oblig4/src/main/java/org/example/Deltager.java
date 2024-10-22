92b0d934bd2a2d30e0b522c838545dbc93aa00e6
h676669 <142335301+h676669@users.noreply.github.com>
2024-10-22 12:38:43 +0200

    deltager klasse

--- Oblig4/src/main/java/org/example/Deltager.java
+++ Oblig4/src/main/java/org/example/Deltager.java
@@ -6,4 +6,52 @@ public class Deltager {
     private String fornavn;
     private String etternavn;
     private String kjonn;
+
+    public Deltager(String mobil, String passord, String fornavn, String etternavn, String kjonn) {
+        this.mobil = mobil;
+        this.passord = passord;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.kjonn = kjonn;
+    }
+
+    public String getPassord() {
+        return passord;
+    }
+
+    public void setPassord(String passord) {
+        this.passord = passord;
+    }
+
+    public String getMobil() {
+        return mobil;
+    }
+
+    public void setMobil(String mobil) {
+        this.mobil = mobil;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getKjonn() {
+        return kjonn;
+    }
+
+    public void setKjonn(String kjonn) {
+        this.kjonn = kjonn;
+    }
 }
