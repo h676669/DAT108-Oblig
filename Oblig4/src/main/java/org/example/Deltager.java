2f305822cbb949b50e59405a78f944120ad4df8c
h676669 <676669@stud.hvl.no>
2024-10-23 21:42:26 +0200

    Update Deltager.java

--- Oblig4/src/main/java/org/example/Deltager.java
+++ Oblig4/src/main/java/org/example/Deltager.java
@@ -1,12 +1,30 @@
 package org.example;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public class Deltager {
+
+    @Pattern(regexp = "^\\d{8}$", message = "Servant: Mobiwnyummew må OwO væwe nøyaktig 8 siffew og kan ikke starte med 0.")
+    @NotNull(message ="Servant: Mobiwnyummew må OwO væwe nøyaktig 8 siffew og kan ikke starte med 0.")
     private String mobil;
+
+    @Size(min = 8)
+    @NotNull(message = "Servant: Passowd må OwO væwe minst 8 tegn.")
     private String passord;
+
+    @Pattern(regexp = "^[A-Za-zæøåÆØÅ\\- ]{2,20}$",message = "Servant: Fownyavn må OwO væwe mewwom 2 og 20 bokstavew.")
+    @NotNull(message = "Servant: Fownyavn må OwO væwe mewwom 2 og 20 bokstavew.")
     private String fornavn;
+
+    @Pattern(regexp ="^[A-Za-zæøåÆØÅ\\- ]{2,20}$", message = "Servant: E-E-Ettewnyavn må OwO væwe mewwom 2 og 20 bokstavew.")
+    @NotNull(message = "Servant:  E-E-Ettewnyavn må OwO væwe mewwom 2 og 20 bokstavew.")
     private String etternavn;
+
+    @NotNull
     private Kjonn kjonn;
 
     public Deltager() {
