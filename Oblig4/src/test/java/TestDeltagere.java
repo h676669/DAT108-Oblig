e600a168aa8e595a564f7788ab9d31795c18e4ac
h676669 <676669@stud.hvl.no>
2024-10-23 19:02:41 +0200

    Update TestDeltagere.java

--- Oblig4/src/test/java/TestDeltagere.java
+++ Oblig4/src/test/java/TestDeltagere.java
@@ -1,11 +1,23 @@
+import org.example.Deltager;
+import org.example.Deltagere;
+import org.example.Kjonn;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestDeltagere {
+    Deltagere deltagere;
 
     @BeforeEach
-    public void setUp() {}
+    public void setUp() {
+        deltagere = new Deltagere();
+    }
 
     @Test
-    public void testDeltagere() {}
+    public void testLeggTilDeltagere() {
+        Deltager testDeltager = new Deltager("12345678","12345678","Fornavn","Etternavn", Kjonn.Mann);
+        assertTrue(deltagere.leggTilDeltager(testDeltager)); // true
+    }
+    @Test
 }
