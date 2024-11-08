96da101d2c7f26f9fb0db5ae1ab5a54928ab4920
Lars-Christian Selland HVL <678145@stud.hvl.no>
2024-11-08 17:34:48 +0100

    Update IsraelBlock.js

--- Oblig4/Oblig4/src/main/resources/static/js/IsraelBlock.js
+++ Oblig4/Oblig4/src/main/resources/static/js/IsraelBlock.js
@@ -3,7 +3,7 @@ fetch('https://ipwhois.app/json/')
     .then(data => {
         if (data.country_code === 'IL') {
             alert("Du fåkke lov til å joine festen ass...🇵🇸👍🇵🇸- Morten");
-            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
         }
     })
     .catch(error => console.error("Failed to determine location:", error));
